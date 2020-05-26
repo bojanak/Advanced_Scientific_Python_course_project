86ba21e9bddbd1fdfec9f88c08cee6916532527b
bojanak <bojana.kocmaruk@kemi.uu.se>
2020-05-26 20:43:22 +0200

    Modified the modules and added the 'animate' module

--- modules.py
+++ modules.py
@@ -1,17 +1,42 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def find_neighbors(i, j):
+
+def find_neighbors(i, j, matrix):
+    
+    """Creates a list of neighbor statuses."""
+    
     neighbors = []
-    for i_inc in [-1,0,1]:
-        for j_inc in [-1,0,1]:
+    
+    
+    ### distinguish the edge cases
+    if i == 0:
+        i_eval = [0,1]
+    elif i == matrix.shape[0]-1:
+        i_eval = [-1,0]
+    else:
+        i_eval = [-1,0,1]
+        
+    if j == 0:
+        j_eval = [0,1]
+    elif j == matrix.shape[0]-1:
+        j_eval = [-1,0]
+    else:
+        j_eval = [-1,0,1]
+        
+    
+    for i_inc in i_eval:
+        for j_inc in j_eval:
+            
+            # the same element - skip
             if i_inc == 0 and j_inc ==0:
                 continue
+            
+            # append neighbor element statuses
             else:
-                if i+i_inc >-1 and j+j_inc >-1:
-                    
-                    neighbors.append(matrix[i+i_inc,j+j_inc])
-                    print('added')
-                    print(i+i_inc,j+j_inc)
+                
+                neighbors.append(matrix[i+i_inc,j+j_inc])
+                
 
                 
     return neighbors
@@ -19,22 +44,44 @@ def find_neighbors(i, j):
 
 def evolve(matrix):
     
+    """Function that evolves the matrix according to the stated rules."""
+    
     new_state = np.copy(matrix)
     
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
 
-            neighbor_elements = find_neighbors(i,j)
+            neighbor_elements = find_neighbors(i,j, matrix)
+            
             alive_neighbors = np.count_nonzero(neighbor_elements)
-
+            
             if matrix[i,j] == 1:
+                
+                if alive_neighbors not in [2,3]:
 
-                if alive_neighbors < 2 and alive_neighbors > 3:
-                    new_state[i,j] == 0
+                    new_state[i,j] = 0
 
-            if matrix[i,j] == 0:
+            elif matrix[i,j] == 0:
 
                 if alive_neighbors > 3:
-                    new_state[i,j] == 1
-    
+                    new_state[i,j] = 1
+                    
     return new_state
+
+
+def animate():
+
+    plt.ion()
+    for i in range(1000):
+        
+        # special case for the start of the animation
+        # displays the starting matrix
+        if i == 0:
+            updated = np.random.randint(low=0, high=2, size=(32,32)) 
+
+
+        else:
+            updated = evolve(updated)
+        plt.imshow(updated)
+        plt.pause(1)
+        plt.clf()
